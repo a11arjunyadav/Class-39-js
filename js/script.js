65d535a3368bcea3742ae41fc9eec99efe6f5665
a11arjunyadav <yadavarjun0312@gmail.com>
2023-08-20 18:58:29 +0530

    js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+//There are 2 thing rleted with function
+
+//1. function defination is a one time process
+// Bad softwere enginner
+ function getmyname(){ //This is not a per camlecase standar
+    //function body3
+    console.log("Arjun")
+ }
+ getmyname();
+ function getMySurname(){ //This function name is as per the camleCase standar
+     //Inside the function body i can write many statment
+     console.log('YADAV'); //statment = instruction
+ }
+
+ //2. function calling/Invoking  call/Invoke
+ getMySurname();
---
+++ js/script2.js
@@ -0,0 +1,10 @@
+
+
+//Lets create a function which can add 2 numbers
+// function defination one time 
+function addTwoNumbers(n1,n2){ 
+    console.log(n1+n2);
+    //every function return somthing 
+} 
+//function calling/Invoking (many time)
+addTwoNumbers(20,30);
